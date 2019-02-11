91c1bbcdf2bb137bb9a7c69412b419e0661825cf
Abdellah Lafia <a.3afia@gmail.com>
2019-02-11 14:31:00 +0400

    mobile compatibily and

--- js/main.js
+++ js/main.js
@@ -1,1 +1,97 @@
-var scrollPos=0,ticking=!1,shouldFade=!1,nav=document.querySelector(".nav"),menu=document.querySelector(".menu"),menuBtn=document.querySelector(".menu-btn"),menuBtnClicked=!1;function openMenu(){menuBtnClicked=(menuBtn.style.transform=menuBtnClicked?(menu.style.display="none",menu.style.overflow="","translateY(-5px)"):(menu.style.display="flex",menu.style.flexFlow="column",menu.style.justifyContent="center",menu.style.alignItems="center",menu.style.height="100vh",menu.style.width="100vw",menu.style.position="absolute",menu.style.overflow="hidden",menu.style.backgroundColor="#fff",menu.style.border="5px solid #000","translateY(5px)"),!menuBtnClicked)}function changeOpacity(e){nav.style.opacity=e?"0":"1"}menuBtn.addEventListener("click",openMenu),window.addEventListener("scroll",function(){scrollPos=(shouldFade=window.scrollY>scrollPos,window.scrollY),ticking||window.requestAnimationFrame(function(){changeOpacity(shouldFade),ticking=!1}),ticking=!0});var strings=document.getElementById("typed-strings"),typedId=document.getElementById("typed"),typed=new Typed(typedId,{stringsElement:strings,typeSpeed:50,startDelay:0,backSpeed:30,smartBackspace:!0,backDelay:700,showCursor:!1,contentType:"html"}),email=document.getElementById("email"),emailBtn=document.getElementById("email-btn");function copy(){document.execCommand("copy")}function select(e){if(document.selection)(n=document.body.createTextRange()).moveToElementText(e),n.select();else if(window.getSelection){var n;(n=document.createRange()).selectNode(e),window.getSelection().empty(),window.getSelection().addRange(n)}}emailBtn.addEventListener("click",function(){select(email),copy()});
+// fade when scrolling down and show when scrolling up
+
+let lastKnownScrollPosition = 0;
+let ticking = false;
+let shouldFade = false;
+
+const nav = document.querySelector('.nav');
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-btn');
+let menuBtnClicked = false;
+
+menuBtn.addEventListener('click', openMenu);
+
+function openMenu() {
+	if (!menuBtnClicked) {
+			menu.style.display = 'flex';
+			menu.style.margin = '0';
+			menu.style.flexFlow = 'column';
+			menu.style.justifyContent = 'center';
+			menu.style.alignItems = 'center';
+			menu.style.height = '100vh';
+			menu.style.width = '100vw';
+			menu.style.position = 'absolute';
+			menu.style.overflow = 'hidden';
+			menu.style.backgroundColor = '#fff';
+			menu.style.border = '5px solid #000';
+			menuBtn.style.transform = 'translateY(5px)', 'translateX(-5px)';
+			menuBtnClicked = !menuBtnClicked;
+		} else {
+			menu.style.display = 'none';
+			menu.style.overflow = '';
+			menuBtn.style.transform = 'translateY(-5px)', 'translateX(5px)';
+			menuBtnClicked = !menuBtnClicked;	
+		}
+}
+const changeOpacity = (fade) => {
+	if (fade) {
+			nav.style.opacity = "0";
+	} else {
+			nav.style.opacity = "1";
+	}
+}
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > lastKnownScrollPosition) {
+	  shouldFade = true;
+	  lastKnownScrollPosition = window.scrollY;
+  } else {
+	  shouldFade = false;
+	  lastKnownScrollPosition = window.scrollY;
+  }
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      changeOpacity(shouldFade);
+      ticking = false;
+    });
+  }
+  ticking = true;
+}); 
+
+// using typed library
+let strings =  document.getElementById("typed-strings");
+let typedId =  document.getElementById("typed");
+
+const typed = new Typed(typedId, {
+    stringsElement: strings,
+	typeSpeed: 50,
+	startDelay: 0,
+	backSpeed: 30,
+	smartBackspace: true,
+	backDelay: 700,
+	//loop: false,
+	//loopCount: Infinity,
+	showCursor: false,
+	contentType: 'html',
+	
+  });
+  
+// copy Email 
+const email = document.getElementById("email");
+const emailBtn = document.getElementById("email-btn");
+
+function copy() {
+	document.execCommand("copy");	
+}
+function select() {
+	email.textContent; 	
+}
+emailBtn.addEventListener("click", ()=> {
+	select();
+	copy();
+});
+
+
+
+
+
