b6998446300daa0db4d8e876a60ef90ea3133981
Abdellah Lafia <a.3afia@gmail.com>
2022-10-29 04:29:20 +1000

    local_server refactor and gitignore

--- local_server.sh
+++ local_server.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-python3 -m http.server 42069
+
+# for local testing
+
+
+echo -e "Listening Port >> [8080[ ... \nLogs >> [server.log] ...\n" && python3 -m http.server 8080 | tee -a server.log
