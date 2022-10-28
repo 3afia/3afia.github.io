0732f86795ec8501a9f568c15695fd809b190ed4
Abdellah Lafia <a.3afia@gmail.com>
2022-10-29 04:30:58 +1000

    refactor local_server

--- local_server.sh
+++ local_server.sh
@@ -3,4 +3,4 @@
 # for local testing
 
 
-echo -e "Listening Port >> [8080[ ... \nLogs >> [server.log] ...\n" && python3 -m http.server 8080 | tee -a server.log
+echo -e "Listening Port >> [8080]... \nLogs >> [server.log] ...\n" && python3 -m http.server 8080 | tee -a server.log
